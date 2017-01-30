05143d8d5a3ead103c9dcb3dd79e82b306f6916f
MD Rahman <mdrahman1472@yahoo.com>
2017-01-30 18:18:54 -0500

    deleted

---
+++ JSONDemo/app/src/main/java/com/example/rahman/jsondemo/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.rahman.jsondemo;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String result;
+        DownloadTask task = new DownloadTask();
+
+        task.execute("http://api.openweathermap.org/data/2.5/weather?q=London");
+
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            try {
+                url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                InputStream in = connection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+                while(data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) { // to show when download is done
+            super.onPostExecute(result);
+            Log.i("res: ", result);
+        }
+    }
+}
